4bbca272eb379a84b28b9af3f0fb6ece676f8974
qiuye <1464815392@qq.com>
2023-03-02 12:52:45 +0800

    chore:添加vercel部署代理设置

--- api/proxy.js
+++ api/proxy.js
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/backend`
             // 例如 /backend/user/login 将被转发到 http://gmall-h5-api.atguigu.cn
-            '^/api': '/',
+            '^/api': '',
         },
     })(req, res)
 }
